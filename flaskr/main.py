d533b8405874f5faa401c3a2a7bb9e46a74e49b8
toshi <toshihiro.mitsutake@gmail.com>
2024-11-14 22:31:32 +0900

    dousa

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,18 +1,39 @@
 from flaskr import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
+import sqlite3
+
+DATABASE = 'database.db'
 
 @app.route('/')
 def index():
-    samples = [
-        {'title': 'test',
-        'number': 1,
-        'create_day': 20241114_2335},
-        {'title': 'testtest',
-        'number': 2,
-        'create_day': 20241114_2337},
-    ]    
+    conn = sqlite3.connect(DATABASE)
+    db_samples = conn.execute('SELECT * FROM samples').fetchall()
+    conn.close()
+    
+    samples = []
+    for row in db_samples:
+        samples.append({'title': row[0], 'number': row[1], 'create_day': row[2]})
+        
     return render_template(
         'index.html',
         samples = samples
     )
 
+@app.route('/form')
+def form_html():
+    return render_template(
+        'form.html'
+    )
+
+@app.route('/regist', methods=['POST'])
+def regist():
+    title = request.form['title']
+    number = request.form['number']
+    create_day = request.form['create_day']
+    
+    conn = sqlite3.connect(DATABASE)
+    conn.execute('INSERT INTO samples VALUES(?, ?, ?)',
+                 [title, number, create_day])
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
