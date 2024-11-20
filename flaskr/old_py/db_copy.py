df59d7aafbf6efbea5227ce78182496a89a7f814
EC2 Default User <ec2-user@amazonlinux.onprem>
2024-11-20 23:41:38 +0900

    古いファイルの削除

--- flaskr/old_py/db_copy.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-DATABASE = 'database.db'
-
-def create_samples_table():
-    conn = sqlite3.connect(DATABASE)
-    conn.execute("CREATE TABLE IF NOT EXISTS samples (title, number, create_day)")
-    conn.close()
