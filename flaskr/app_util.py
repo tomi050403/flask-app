ca3e6e7238eb40b63dfd61f4a9f2e105223b351e
EC2 Default User <ec2-user@amazonlinux.onprem>
2024-12-15 20:00:32 +0900

    app_utill.pyの誤字を修正

--- flaskr/app_util.py
+++ flaskr/app_util.py
@@ -4,7 +4,7 @@ import os
 
 def db_create():
     load_dotenv()
-    connection = mysql.connectory.connect(
+    connection = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
