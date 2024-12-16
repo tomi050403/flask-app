fa1a3503d98baf31eb155da35c998bcba93bd341
EC2 Default User <ec2-user@amazonlinux.onprem>
2024-12-16 21:28:50 +0900

    db_createを追加

--- flaskr/app_util.py
+++ flaskr/app_util.py
@@ -17,7 +17,7 @@ def db_connect():
         host = os.getenv('DB_HOST'),
         user = os.getenv('DB_USER'),
         password = os.getenv('DB_PASSWORD'),
-        database = os.getenv('TABLE_NAME')
+        database = os.getenv('DB_NAME')
     )
     return connection
 
--- flaskr/db.py
+++ flaskr/db.py
@@ -14,14 +14,14 @@ def create_database():
         print("環境変数 DB_NAME を設定して下さい。")
     conn = db_create()
     cursor = conn.cursor()
-    create_db_sql = """
+    create_db_sql = f"""
     CREATE DATABASE IF NOT EXISTS `{db_name}`
     """
     try:
-        cursor.execute(create_database)
+        cursor.execute(create_db_sql)
         print("データベースが正常に作成されました")
-    except:
-        print("データベース作成に失敗しました")
+    except mysql.connector.Error as err:
+        print(f"データベース作成に失敗しました: {err}")
     finally:
         cursor.close()
         conn.close()
@@ -30,7 +30,7 @@ def create_samples_table():
     conn = db_connect()
     cursor = conn.cursor()
     table_name = os.getenv("TABLE_NAME")
-    create_table_sql = """
+    create_table_sql = f"""
     CREATE TABLE IF NOT EXISTS `{table_name}` (
         id INT AUTO_INCREMENT PRIMARY KEY,
         filename VARCHAR(100),
@@ -39,7 +39,7 @@ def create_samples_table():
         update_day DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
     )
     """
-    # [table_name,]
+
 
     cursor.execute(create_table_sql)
     cursor.close()
