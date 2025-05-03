71a8fe46465a80f841767d8502026f8d403c23a5
tomi050403 <toshihiro.mitsutake@gmail.com>
2025-05-03 22:44:35 +0900

    - mysql_connection_error 500 の機能の追加 - flask 脆弱性対応に伴うアップデート

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,5 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 from flaskr import main,db,app_util
-db.create_database()
-db.create_samples_table()
+
+try:
+    db.create_database()
+    db.create_samples_table()
+except Exception as e:
+    print(f"DB_initian_Error: {e}")
